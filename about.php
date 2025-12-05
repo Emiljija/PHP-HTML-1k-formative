09a2621e75b53974bb1beb7d0cc84563b9c89548
you <you@gmail.com>
2025-12-05 08:51:58 +0200

    Added html boilerplate to about.php, added an indexed array.

--- about.php
+++ about.php
@@ -1,1 +1,21 @@
-<!-- your code goes here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    echo "<h1>About</h1>";
+
+    $indexedArray = ["Lorem ipsum dolor sit amet.", "Lorem ipsum dolor sit amet consectetur adipisicing.", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, obcaecati?"];
+
+    foreach($indexedArray as $str)
+        echo "<p>$str</p>";
+    
+
+    ?>
+</body>
+</html>
